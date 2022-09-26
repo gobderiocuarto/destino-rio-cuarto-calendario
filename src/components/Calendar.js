777a48db6230268ee9bab22886d94081db6065da
germansayago <german.sayago@gmail.com>
2022-09-26 14:04:37 -0300

    fix imports

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,9 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import esLocale from "@fullcalendar/core/locales/es";
-import Spinner from "./Spinner";
 
 export default function Calendar(events) {
   const spinner = useRef(null);
@@ -14,10 +13,8 @@ export default function Calendar(events) {
       if (spinner.current) {
         if (state) {
           spinner.current.classList.remove("d-none");
-          // console.log("remove d-none");
         } else {
           spinner.current.classList.add("d-none");
-          // console.log("add d-none");
         }
       }
     },
@@ -32,9 +29,6 @@ export default function Calendar(events) {
   };
   return (
     <>
-      {/* <div ref={spinner} className="">
-        <Spinner />
-      </div> */}
       <FullCalendar
         plugins={[interactionPlugin, dayGridPlugin, timeGridPlugin]}
         headerToolbar={{
